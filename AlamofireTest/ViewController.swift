9f6214217e5d9ef6ab92e4efe875d90a222a2b97
sjc-bui <sjc-bui@gmail.com>
2021-05-02 12:25:30 +0900

    add authen method

--- AlamofireTest/ViewController.swift
+++ AlamofireTest/ViewController.swift
@@ -33,9 +33,28 @@ class ViewController: UIViewController {
     }
 
     func anotherMethod() {
-        let url = "https://httpbin.org/"
-        AF.request(url + "post", method: .post) // post
-        AF.request(url + "put", method: .put) // put
-        AF.request(url + "delete", method: .delete) // delete
+//        let url = "https://httpbin.org/"
+//        AF.request(url + "post", method: .post) // post
+//        AF.request(url + "put", method: .put) // put
+//        AF.request(url + "delete", method: .delete) // delete
+    }
+
+    func requetWithHeader(url: String, params: Dictionary<String, Any>) {
+        let headers: HTTPHeaders = [
+            .authorization(username: "test", password: "test"),
+            .accept("application/json")
+        ]
+        AF.request(url, parameters: params, headers: headers).responseJSON { (response) in
+            debugPrint(response)
+        }
+    }
+    
+    func requestAuthen() {
+        let credential = URLCredential(user: "test", password: "test", persistence: .forSession)
+        AF.request("url")
+            .authenticate(with: credential)
+            .responseJSON { (response) in
+                debugPrint(response)
+            }
     }
 }
